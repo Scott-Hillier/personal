42b3978bdd61c0fc25a1ca9af7513dd08382b18d
Scott Hillier <scotthhillier@gmail.com>
2022-08-22 14:33:31 -0700

    initialized bootstrap

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,17 @@
-import "./App.css";
+import "./App.scss";
 import Description from "./components/description";
+import Navigation from "./components/navigation";
+import Button from "react-bootstrap/Button";
+import profile from "./images/Profile.jpeg";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Description />
       <header className="App-header">
+        <img src={profile} className="profile" alt="logo" />
+        <Button>This is a button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

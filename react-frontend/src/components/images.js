e7b0eace927f5f9fe860f58a24fde05de2b3ba37
Scott Hillier <scotthhillier@gmail.com>
2022-08-23 12:07:16 -0700

    implemented react image gallery

--- react-frontend/src/components/images.js
+++ react-frontend/src/components/images.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ImageGallery from "react-image-gallery";
 
 let index = 0;
 const Images = () => {
@@ -8,31 +9,54 @@ const Images = () => {
   const images = importAll(
     require.context("../images", false, /\.(png|jpe?g|svg)$/)
   );
-  const [imageState, setImageState] = useState(images[0]);
+  const slideshow = [];
+  images.map((image) => {
+    slideshow.push({ original: image });
+  });
   return (
-    <section className="images">
-      <div>
-        <i
-          className="bi bi-arrow-left-circle-fill"
-          onClick={() => {
-            index === 0 ? (index = images.length - 1) : index--;
-            setImageState(images[index]);
-          }}
-        ></i>
-        <img
-          className="image"
-          src={imageState}
-          alt="Strange, there should be photos of me here"
-        ></img>
-        <i
-          className="bi bi-arrow-right-circle-fill"
-          onClick={() => {
-            index === images.length - 1 ? (index = 0) : index++;
-            setImageState(images[index]);
-          }}
-        ></i>
-      </div>
-    </section>
+    <ImageGallery
+      items={slideshow}
+      autoPlay
+      slideInterval={5000}
+      swipingTransitionDuration={500}
+    />
+    // <section className="images">
+    //   <div>
+    //     <img
+    //       className="image"
+    //       src={imageState}
+    //       alt="Strange, there should be photos of me here"
+    //     ></img>
+    //   </div>
+    //   <div>
+    //     <i
+    //       className="bi bi-arrow-left-circle-fill"
+    //       onClick={() => {
+    //         index === 0 ? (index = images.length - 1) : index--;
+    //         setImageState(images[index]);
+    //       }}
+    //     ></i>
+    //     <img
+    //       className="image"
+    //       src={imageState}
+    //       alt="Strange, there should be photos of me here"
+    //     ></img>
+    //     <i
+    //       className="bi bi-arrow-right-circle-fill"
+    //       onClick={() => {
+    //         index === images.length - 1 ? (index = 0) : index++;
+    //         setImageState(images[index]);
+    //       }}
+    //     ></i>
+    //   </div>
+    //   <div>
+    //     <img
+    //       className="image"
+    //       src={imageState}
+    //       alt="Strange, there should be photos of me here"
+    //     ></img>
+    //   </div>
+    // </section>
   );
 };
 

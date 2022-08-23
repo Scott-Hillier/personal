22f64cc47b11beb02dc7d61d9d310798ba500777
Scott Hillier <scotthhillier@gmail.com>
2022-08-22 18:11:29 -0700

    photo slideshow now operational

--- react-frontend/src/components/images.js
+++ react-frontend/src/components/images.js
@@ -1,28 +1,39 @@
-// import photos from "../images/";
-import photo from "../images/Profile.jpeg";
+import { useState } from "react";
 
+let index = 0;
 const Images = () => {
-  console.log("PHOTO", photo);
   function importAll(r) {
     return r.keys().map(r);
   }
-
   const images = importAll(
     require.context("../images", false, /\.(png|jpe?g|svg)$/)
   );
-
+  const [imageState, setImageState] = useState(images[0]);
   return (
-    <section>
-      {images.map((imgSrc, index) => {
-        return (
-          <img
-            className="image"
-            src={imgSrc}
-            key={index}
-            alt="Strange, there should be photos of me here"
-          />
-        );
-      })}
+    <section className="images">
+      <div>
+        <i
+          className="bi bi-arrow-left-circle-fill"
+          onClick={() => {
+            index === 0 ? (index = images.length - 1) : index--;
+            setImageState(images[index]);
+            console.log("index", index);
+          }}
+        ></i>
+        <img
+          className="image"
+          src={imageState}
+          alt="Strange, there should be photos of me here"
+        ></img>
+        <i
+          className="bi bi-arrow-right-circle-fill"
+          onClick={() => {
+            index === images.length - 1 ? (index = 0) : index++;
+            setImageState(images[index]);
+            console.log("index", index);
+          }}
+        ></i>
+      </div>
     </section>
   );
 };

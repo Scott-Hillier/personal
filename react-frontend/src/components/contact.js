94c572f1a33aac9ca5a6aa512356d179df38c111
Scott Hillier <scotthhillier@gmail.com>
2022-09-14 12:13:40 -0700

    icons now in place

--- react-frontend/src/components/contact.js
+++ react-frontend/src/components/contact.js
@@ -7,24 +7,47 @@ const Contact = () => {
   const images = importAll(
     require.context("../icons", false, /\.(png|jpe?g|svg)$/)
   );
+  const sources = [
+    "https://www.linkedin.com/in/scott-hillier-11aaa5197/",
+    "https://github.com/Scott-Hillier",
+  ];
   const icons = [];
-  images.map((image) => {
+  images.map((image, i) => {
     icons.push({
       original: image,
+      href: sources[i],
     });
   });
   return (
     <section className="icons">
-      {icons.map((icon, i) => {
-        return (
-          <img
-            className="icon"
-            src={icon.original}
-            alt="Strange, there should be an icon here"
-            key={i}
-          ></img>
-        );
-      })}
+      <a href="https://www.linkedin.com/in/scott-hillier-11aaa5197/">
+        <img
+          className="icon"
+          src="/static/media/01_linkedin.032cf8a63be2f0f4d425.png"
+          alt="Strange, there should be an icon here"
+        />
+      </a>
+      <a href="https://github.com/Scott-Hillier">
+        <img
+          className="icon"
+          src="/static/media/02_github.f11021803f68852df283.png"
+          alt="Strange, there should be an icon here"
+        />
+      </a>
+      <a href="https://github.com/Scott-Hillier">
+        <img
+          className="icon"
+          src="/static/media/03_email.67bda94140af311d041f.png"
+          alt="Strange, there should be an icon here"
+        />
+      </a>
+      <a href="https://github.com/Scott-Hillier">
+        <img
+          className="icon"
+          src="/static/media/04_phone.2fa539562c029c1d51b6.png"
+          alt="Strange, there should be an icon here"
+        />
+      </a>
     </section>
   );
 };

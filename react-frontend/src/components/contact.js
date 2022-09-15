5a05af8bb90d7ec9fa56025b9bf7a1c332bdc6a6
Scott Hillier <scotthhillier@gmail.com>
2022-09-15 14:38:22 -0700

    contact icons now load properly on first render

--- react-frontend/src/components/contact.js
+++ react-frontend/src/components/contact.js
@@ -1,26 +1,37 @@
 import "../styles/contact.scss";
 
 const Contact = () => {
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+  const images = importAll(
+    require.context("../images/icons", false, /\.(png|jpe?g|svg)$/)
+  );
+  const icons = [];
+  images.map((image, i) => {
+    icons.push(image);
+  });
+
   return (
     <section className="icons">
       <a href="https://www.linkedin.com/in/scott-hillier-11aaa5197/">
         <img
           className="icon"
-          src="/static/media/01_linkedin.032cf8a63be2f0f4d425.png"
+          src={icons[0]}
           alt="Strange, there should be an icon here"
         />
       </a>
       <a href="https://github.com/Scott-Hillier">
         <img
           className="icon"
-          src="/static/media/02_github.f11021803f68852df283.png"
+          src={icons[1]}
           alt="Strange, there should be an icon here"
         />
       </a>
       <a href="mailto: scotthhillier@gmail.com">
         <img
           className="icon"
-          src="/static/media/03_email.67bda94140af311d041f.png"
+          src={icons[2]}
           alt="Strange, there should be an icon here"
         />
       </a>
@@ -30,7 +41,7 @@ const Contact = () => {
             alert("613-402-5186");
           }}
           className="icon"
-          src="/static/media/04_phone.2fa539562c029c1d51b6.png"
+          src={icons[3]}
           alt="Strange, there should be an icon here"
         />
       </a>

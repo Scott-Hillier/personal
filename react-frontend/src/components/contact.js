a1fc4de92c19a3688446a727d8ae627bc53d0ff7
Scott Hillier <scotthhillier@gmail.com>
2022-09-20 14:58:02 -0700

    updated contact images to icons

--- react-frontend/src/components/contact.js
+++ react-frontend/src/components/contact.js
@@ -1,49 +1,19 @@
 import "../styles/contact.scss";
 
 const Contact = () => {
-  function importAll(r) {
-    return r.keys().map(r);
-  }
-  const images = importAll(
-    require.context("../images/icons", false, /\.(png|jpe?g|svg)$/)
-  );
-  const icons = [];
-  images.map((image, i) => {
-    icons.push(image);
-  });
-
   return (
     <section className="icons">
       <a href="https://www.linkedin.com/in/scott-hillier-11aaa5197/">
-        <img
-          className="icon"
-          src={icons[0]}
-          alt="Strange, there should be an icon here"
-        />
+        <i className="bi bi-linkedin icon"></i>
       </a>
       <a href="https://github.com/Scott-Hillier">
-        <img
-          className="icon"
-          src={icons[1]}
-          alt="Strange, there should be an icon here"
-        />
+        <i className="bi bi-github icon"></i>
       </a>
       <a href="mailto: scotthhillier@gmail.com">
-        <img
-          className="icon"
-          src={icons[2]}
-          alt="Strange, there should be an icon here"
-        />
+        <i className="bi bi-envelope-fill icon"></i>
       </a>
       <a>
-        <img
-          onClick={() => {
-            alert("613-402-5186");
-          }}
-          className="icon"
-          src={icons[3]}
-          alt="Strange, there should be an icon here"
-        />
+        <i className="bi bi-telephone-fill icon"></i>
       </a>
     </section>
   );

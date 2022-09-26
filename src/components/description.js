0ab346f91635a481323f6bad90ea90230b14fa75
Scott-Hillier <scotthhillier@gmail.com>
2022-09-26 09:58:57 -0700

    updated personal description

--- src/components/description.js
+++ src/components/description.js
@@ -2,11 +2,10 @@ const Description = () => {
   return (
     <section id="about">
       <p>
-        My name is Scott Hillier. I grew up in Ottawa and attended Carleton
-        University where I earned a bachelor's degree in aerospace engineering.
-        I was first introduced to coding during my time at Carleton. Wanting to
-        improve my skillset, I enrolled and completed the Lighthouse Labs web
-        development bootcamp.
+        My name is Scott Hillier. I attended Carleton University where I earned
+        a bachelor's degree in aerospace engineering. I was first introduced to
+        coding during my time at Carleton. Wanting to improve my skillset, I
+        enrolled and completed the Lighthouse Labs web development bootcamp.
       </p>
       <br />
       <p>Below are some of the projects that I've worked on.</p>
